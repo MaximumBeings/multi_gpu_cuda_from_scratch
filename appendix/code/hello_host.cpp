cb25ce5f7359efc2dead9a05f40556b774cfe932
Oluwaseyi Awoga <oawoga1@gmail.com>
2026-09-18 18:33:45 -0400

    Deployed c6ac2a9 with MkDocs version: 1.6.1

---
+++ appendix/code/hello_host.cpp
@@ -0,0 +1,6 @@
+// Appendix A.4: Recipe 1 stub -- plain host C++, no CUDA at all.
+#include <cstdio>
+int main() {
+    printf("hello_host: compiled with plain g++, no CUDA involved.\n");
+    return 0;
+}
