0b5eb4b6d35073725e69e3c26da5ccc7b983dbfb
Oluwaseyi Awoga <oawoga1@gmail.com>
2026-09-18 18:39:21 -0400

    Deployed c6ac2a9 with MkDocs version: 1.6.1

--- appendix/code/hello_host.cpp
+++
@@ -1,6 +0,0 @@
-// Appendix A.4: Recipe 1 stub -- plain host C++, no CUDA at all.
-#include <cstdio>
-int main() {
-    printf("hello_host: compiled with plain g++, no CUDA involved.\n");
-    return 0;
-}
--- appendix/code/hello_nccl.cu
+++
@@ -1,8 +0,0 @@
-// Appendix A.4: Recipe 3 stub -- nvcc + NCCL headers/link line.
-#include <cstdio>
-#include <nccl.h>
-int main() {
-    printf("hello_nccl: compiled and linked against real libnccl "
-           "(NCCL %d.%d.%d).\n", NCCL_MAJOR, NCCL_MINOR, NCCL_PATCH);
-    return 0;
-}
